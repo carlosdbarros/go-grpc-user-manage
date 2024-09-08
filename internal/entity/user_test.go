6b0470b6995d24e4468005ab4cab4717bba45193
Carlos Daniel Barros <barros.carlosds@gmail.com>
2024-09-08 12:35:53 -0300

    test: Add setup and teardown

--- internal/entity/user_test.go
+++ internal/entity/user_test.go
@@ -1,29 +1,70 @@
 package entity
 
 import (
+	"fmt"
+	"github.com/go-faker/faker/v4"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
-func makeUser() (*User, error) {
-	return NewUser("Carlos", "t@t.com", "123456")
+func TestMain(m *testing.M) {
+	println("#################### STARTING TESTS ####################")
+	println("Before all tests")
+	code := m.Run()
+	println("After all tests")
+	println("#################### FINISHED TESTS ####################")
+	os.Exit(code)
+}
+
+func setupTest() func() {
+	setup()
+	return teardown
+}
+
+var (
+	user     *User
+	err      error
+	name     = ""
+	email    = ""
+	password = ""
+)
+
+func setup() {
+	println("Before each test")
+	name = faker.Name()
+	email = faker.Email()
+	password = faker.Password()
+	user, err = NewUser(name, email, password)
+	fmt.Printf("User created: %v\n", user)
+
+}
+
+func teardown() {
+	println("After each test")
+	user = nil
+	err = nil
+	name = ""
+	email = ""
+	password = ""
+	println("--------------------")
 }
 
 func TestNewUser(t *testing.T) {
-	user, err := makeUser()
+	defer setupTest()()
 	assert.Nil(t, err)
 	assert.NotNil(t, user)
 	assert.NotEmpty(t, user.ID)
 	assert.NotEmpty(t, user.Password)
-	assert.Equal(t, "Carlos", user.Name)
-	assert.Equal(t, "t@t.com", user.Email)
+	assert.Equal(t, name, user.Name)
+	assert.Equal(t, email, user.Email)
 }
 
 func TestUser_ValidatePassword(t *testing.T) {
-	user, err := makeUser()
+	defer setupTest()()
 	assert.Nil(t, err)
 	assert.NotNil(t, user)
-	assert.True(t, user.ValidatePassword("123456"))
-	assert.False(t, user.ValidatePassword("1234567"))
-	assert.NotEqual(t, "123456", user.Password)
+	assert.True(t, user.ValidatePassword(password))
+	assert.False(t, user.ValidatePassword("123"))
+	assert.NotEqual(t, password, user.Password)
 }
