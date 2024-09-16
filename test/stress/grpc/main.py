4c8b66919585f72fe493d61311a330fc5dfdbde5
Carlos Daniel Barros <barros.carlosds@gmail.com>
2024-09-16 01:17:41 -0300

    test: Add http load test

--- test/stress/grpc/main.py
+++ test/stress/grpc/main.py
@@ -2,30 +2,18 @@ import logging
 import time
 import string
 import grpc
+import random
 
 from typing import Any, Callable
 
 import grpc.experimental.gevent as grpc_gevent
 from grpc_interceptor import ClientInterceptor
-
 from locust import User, task
-from locust.exception import LocustError
 
 from user import user_pb2, user_pb2_grpc
 
 
 logger = logging.getLogger(__name__)
-
-
-class UserServiceServicer(user_pb2_grpc.UserServiceServicer):
-    def CreateUser(self, request, context):
-        user = user_pb2.User(
-            id="1",
-            name=request.name,
-            email=request.email,
-        )
-        return user
-
 
 # patch grpc so that it uses gevent instead of asyncio
 grpc_gevent.init_gevent()
@@ -36,7 +24,12 @@ class LocustInterceptor(ClientInterceptor):
         super().__init__(*args, **kwargs)
         self.env = environment
 
-    def intercept(self, method: Callable, request_or_iterator: Any, call_details: grpc.ClientCallDetails):
+    def intercept(
+        self,
+        method: Callable,
+        request_or_iterator: Any,
+        call_details: grpc.ClientCallDetails,
+    ):
         response = None
         exception = None
         start_perf_counter = time.perf_counter()
@@ -60,16 +53,9 @@ class LocustInterceptor(ClientInterceptor):
         return response
 
 
-class gRPCClient(User):
-    abstract = True
-    stub_class = None
-
-    def __init__(self, environment):
-        super().__init__(environment)
-        required_attrs = ["host", "stub_class"]
-        missing_attrs = [attr for attr in required_attrs if not hasattr(self, attr)]
-        if missing_attrs:
-            raise LocustError(f"Required attributes {missing_attrs} not set on {self.__class__.__name__}")
+class GRPCUserService(User):
+    host = "localhost:50051"
+    stub_class = user_pb2_grpc.UserServiceStub
 
     def on_start(self):
         super().on_start()
@@ -82,21 +68,15 @@ class gRPCClient(User):
         self._channel.close()
         super().on_stop()
 
-
-def generate_random_string(length):
-    return "".join([string.ascii_letters[i % len(string.ascii_letters)] for i in range(length)])
-
-
-class gRPCUserClient(gRPCClient):
-    host = "localhost:50051"
-    stub_class = user_pb2_grpc.UserServiceStub
+    def _random_string(self, length):
+        return "".join(random.choices(string.ascii_letters, k=length))
 
     @task
-    def CreateUser(self):
-        request = user_pb2.CreateUserRequest(
-            name=generate_random_string(10),
-            email=generate_random_string(10) + "@gmail.com",
-            password=generate_random_string(10),
+    def create_user(self):
+        rd_str = self._random_string(10)
+        user_input = user_pb2.CreateUserRequest(
+            name=rd_str,
+            email=rd_str + "@gmail.com",
+            password=rd_str,
         )
-        response = self.stub.CreateUser(request)
-        logger.info(f"CreateUser: {response}")
+        self.stub.CreateUser(user_input)
