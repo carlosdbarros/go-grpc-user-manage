d90f3213b49aa56fbbc5a49c97c2b4046b2aca00
Carlos Daniel Barros <barros.carlosds@gmail.com>
2024-09-25 23:59:08 -0300

    test: Add CreateUserAddress. - Melhores resultados até o momento.

--- test/locust/locustfile.py
+++ test/locust/locustfile.py
@@ -1,19 +1,32 @@
 # test/locust/locust_grpc.py
 import logging
 import time
+from functools import wraps
+
 import grpc
-import grpc._common
-from locust import FastHttpUser, User, task, events, between
+from locust import FastHttpUser, HttpUser, User, task, events, between
+from locust.exception import LocustError
 
 from user import user_pb2_grpc, user_pb2
 
-from utils import generate_user, generate_user_request, LocustClientInterceptor
+from utils import (
+    generate_user,
+    generate_user_address,
+    generate_user_address_request,
+    generate_user_request,
+    generate_user_iterator,
+)
 
+from gevent import monkey
 import grpc.experimental.gevent as grpc_gevent
 
 # patch grpc so that it uses gevent instead of asyncio
+monkey.patch_all()
 grpc_gevent.init_gevent()
+
 logger = logging.getLogger(__name__)
+
+BATCH_SIZE = 10
 
 
 class GrpcUser(User):
@@ -24,95 +37,102 @@ class GrpcUser(User):
     host = None
     stub_class = None
 
-    def __init__(self, environment, *args, **kwargs):
-        super().__init__(environment, *args, **kwargs)
+    def __init__(self, environment):
+        super().__init__(environment)
         for attr in ["host", "stub_class"]:
             if not getattr(self, attr):
-                raise AttributeError(
-                    f"{attr} is required for {self.__class__.__name__}"
+                raise LocustError(
+                    f"Class {self.__class__.__name__} missing required attribute {attr}"
                 )
 
         self._channel = grpc.insecure_channel(self.host.lstrip("http://"))
-        grpc.channel_ready_future(self._channel).result(timeout=15)
+        # grpc.channel_ready_future(self._channel).result(timeout=15)
         self.stub = self.stub_class(self._channel)
-        interceptor = LocustClientInterceptor(environment=environment)
-        self._channel_intercept = grpc.intercept_channel(self._channel, interceptor)
-        self.stub_intercept = self.stub_class(self._channel_intercept)
+        # interceptor = LocustClientInterceptor(environment=environment)
+        # self._channel = grpc.intercept_channel(self._channel, interceptor)
+        # self.stub = self.stub_class(self._channel)
+        self._channel_closed = False
 
-    def on_stop(self):
-        self._channel_intercept.close()
+    def on_start(self):
+        self._channel_closed = False
+
+    def on_stop(self, force=False):
+        self._channel_closed = True
+        time.sleep(1)
         self._channel.close()
-        super().on_stop()
+        super().stop(force=True)
 
 
-def generate_user_iterator(size=1):
-    for _ in range(size):
-        yield generate_user_request(generate_user())
+def grpc_decorator(rpc_name=None):
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(self, *args, **kwargs):
+            if self._channel_closed:
+                return
+            response = None
+            exception = None
+            start_time = time.time()
+            start_perf_counter = time.perf_counter()
+
+            try:
+                response = fn(self, *args, **kwargs)
+            except grpc.RpcError as e:
+                exception = e
+
+            response_time = (time.perf_counter() - start_perf_counter) * 1000
+            response_size = response.ByteSize() if response else 0
+            name = rpc_name.split("/")[-1] if rpc_name else fn.__name__
+
+            self.environment.events.request.fire(
+                request_type="gRPC",
+                response_time=response_time,
+                name=name,
+                response=response,
+                response_length=response_size,
+                start_time=start_time,
+                url=rpc_name,
+                exception=exception,
+                context={},
+            )
+            return response
+
+        return wrapper
+
+    return decorator
 
 
-BATCH_SIZE = 10
-
-class GrpcCreateUser(GrpcUser):
-    host = "http://localhost:50051"
+class GRPCLoadTest(GrpcUser):
+    host = "localhost:50051"
     stub_class = user_pb2_grpc.UserServiceStub
 
     @task
+    @grpc_decorator("grpc.user.UserService/CreateUser")
     def create_user_unary(self):
-        self.stub_intercept.CreateUser(generate_user_request(generate_user()))
+        return self.stub.CreateUser(generate_user_request(generate_user()))
 
     @task
-    def create_user_unary_without_interceptor(self):
-        fn_name = "create_user_unary_without_interceptor"
-        response = None
-        exception = None
-        response_size = 0
-        start_time = time.perf_counter()
-        try:
-            response = self.stub.CreateUser(generate_user_request(generate_user()))
-            response_size = response.ByteSize()
-        except grpc.RpcError as e:
-            exception = e
-        self.environment.events.request.fire(
-            request_type="gRPC",
-            name=fn_name,
-            response_time=(time.perf_counter() - start_time) * 1000,
-            response=response,
-            response_length=response_size,
-            exception=exception,
+    @grpc_decorator("grpc.user.UserService/CreateUserAddress")
+    def create_user_address_unary(self):
+        return self.stub.CreateUserAddress(
+            generate_user_address_request(generate_user_address())
         )
 
-    @task
-    def create_user_stream_stream(self):
-        self.stub_intercept.CreateUserStream(generate_user_iterator())
+
+class HttpLoadTest(HttpUser):
+    min_wait = 1000
+    max_wait = 10000
+    host = "http://localhost:8080"
+
+    # @task
+    # def create_user(self):
+    #     self.client.post("/users", json=generate_user())
 
     @task
-    def create_user_unary_batch(self):
-        for _ in range(BATCH_SIZE):
-            self.stub_intercept.CreateUser(generate_user_request(generate_user()))
+    def create_user_address(self):
+        self.client.post("/users-address", json=generate_user_address())
 
-    @task
-    def create_user_stream_stream_batch(self):
-        for _ in self.stub_intercept.CreateUserStream(generate_user_iterator(BATCH_SIZE)):
-            pass
 
-    @task
-    def create_user_stream_stream_batch_without_interceptor(self):
-        exception = None
-        response_size = 0
-        start_time = time.perf_counter()
-        for response in self.stub.CreateUserStream(generate_user_iterator(BATCH_SIZE)):
-            logger.info(response)
-            self.environment.events.request.fire(
-                request_type="gRPC",
-                name="create_user_stream_stream_batch",
-                response_time=(time.perf_counter() - start_time) * 1000,
-                response=response,
-                response_length=response_size,
-                exception=exception,
-            )
-            start_time = time.perf_counter()
-
-class HttpCreateUser(FastHttpUser):
+class FastHttpLoadTest(FastHttpUser):
     min_wait = 1000
     max_wait = 10000
     host = "http://localhost:8080"
@@ -121,8 +141,11 @@ class HttpCreateUser(FastHttpUser):
     def create_user(self):
         self.client.post("/users", json=generate_user())
 
-
     @task
-    def create_user_batch(self):
-        for _ in range(BATCH_SIZE):
-            self.client.post("/users", json=generate_user())
+    def create_user_address(self):
+        self.client.post("/users-address", json=generate_user_address())
+
+    # @task
+    # def create_user_batch(self):
+    #     for _ in range(BATCH_SIZE):
+    #         self.client.post("/users", json=generate_user())
--- test/locust/utils.py
+++ test/locust/utils.py
@@ -1,5 +1,6 @@
-import logging
 import time
+import string
+import logging
 import grpc
 import faker
 import random
@@ -12,32 +13,90 @@ from user import user_pb2
 faker = faker.Faker()
 logger = logging.getLogger(__name__)
 
+STR_SIZE = 300
+LIST_SIZE = 30
+USERS_SIZE = 1
+USE_FAKER = False
+
+STATIC_STR = "".join(random.choices(string.ascii_letters, k=STR_SIZE))
+STATIC_EMAIL = f"{STATIC_STR}@teste.com"
 
 users = [
     {
-        "name": faker.name(),
-        "email": faker.email(),
-        "password": faker.password(),
+        "name": faker.name() if USE_FAKER else STATIC_STR,
+        "email": faker.email() if USE_FAKER else STATIC_EMAIL,
+        "password": STATIC_STR,
     }
-    for _ in range(10)
+    for _ in range(USERS_SIZE)
+]
+
+usersAddresses = [
+    {
+        "name": faker.name() if USE_FAKER else STATIC_STR,
+        "emails": [
+            faker.email() if USE_FAKER else STATIC_EMAIL for _ in range(LIST_SIZE)
+        ],
+        "phones": [
+            faker.phone_number() if USE_FAKER else STATIC_STR for _ in range(LIST_SIZE)
+        ],
+        "addresses": [
+            {
+                "street": faker.street_name() if USE_FAKER else STATIC_STR,
+                "number": faker.building_number() if USE_FAKER else STATIC_STR,
+                "complement": faker.secondary_address() if USE_FAKER else STATIC_STR,
+                "city": faker.city() if USE_FAKER else STATIC_STR,
+                "state": faker.state() if USE_FAKER else STATIC_STR,
+                "country": faker.country() if USE_FAKER else STATIC_STR,
+                "zipCode": faker.zipcode() if USE_FAKER else STATIC_STR,
+            }
+            for _ in range(LIST_SIZE)
+        ],
+    }
+    for _ in range(USERS_SIZE)
 ]
 
 
+def generate_user_address():
+    return random.choice(usersAddresses)
+    # return {
+    #     "name": STATIC_STR,
+    #     "emails": [ f"{STATIC_STR}@teste.com" for _ in range(LIST_SIZE)],
+    #     "phones": [ STATIC_STR for _ in range(LIST_SIZE)],
+    #     "addresses": [
+    #         {
+    #             "street": STATIC_STR,
+    #             "number": STATIC_STR,
+    #             "complement": STATIC_STR,
+    #             "city": STATIC_STR,
+    #             "state": STATIC_STR,
+    #             "country": STATIC_STR,
+    #             "zipCode": STATIC_STR,
+    #         }
+    #         for _ in range(LIST_SIZE)
+    #     ],
+    # }
+
+
+def generate_user_address_request(user_address):
+    return user_pb2.CreateUserAddressRequest(**user_address)
+
+
 def generate_user():
-    return {
-        "name": "Test User",
-        "email": "t@t.com",
-        "password": "123"
-    }
+    return random.choice(users)
+    # return {
+    #     "name": STATIC_STR,
+    #     "email": f"{STATIC_STR}@teste.com",
+    #     "password": "123456",
+    # }
 
 
 def generate_user_request(user):
-    return user_pb2.CreateUserRequest(
-        name=user["name"],
-        email=user["email"],
-        password=user["password"],
-    )
+    return user_pb2.CreateUserRequest(**user)
 
+
+def generate_user_iterator(size=1):
+    for _ in range(size):
+        yield generate_user_request(generate_user())
 
 
 class LocustClientInterceptor(
@@ -52,8 +111,10 @@ class LocustClientInterceptor(
         exception = None
         response_size = 0
         start_time = time.perf_counter()
+        total_time = 0
         try:
             future = continuation(client_call_details, request_or_iterator)
+            total_time = (time.perf_counter() - start_time) * 1000
         except grpc.RpcError as e:
             exception = e
         else:
@@ -65,7 +126,7 @@ class LocustClientInterceptor(
         self.env.events.request.fire(
             request_type="gRPC",
             name=client_call_details.method,
-            response_time=(time.perf_counter() - start_time) * 1000,
+            response_time=total_time,
             response=response,
             response_length=response_size,
             exception=exception,
@@ -75,5 +136,7 @@ class LocustClientInterceptor(
     def intercept_unary_unary(self, continuation, client_call_details, request):
         return self.intercept_call(continuation, client_call_details, request)
 
-    def intercept_stream_stream(self, continuation, client_call_details, request_iterator):
+    def intercept_stream_stream(
+            self, continuation, client_call_details, request_iterator
+    ):
         return self.intercept_call(continuation, client_call_details, request_iterator)
